e5821c3473f936c0ef710d96a3e2c891de950717
Ryan <92072969+RyanxLowz@users.noreply.github.com>
2024-04-24 22:43:07 +0800

    Add calculateHash to Block

--- Block.java
+++ Block.java
@@ -1,16 +1,28 @@
 import java.util.Date;
 
+import Utils.StringUtil;
+
 public class Block {
 
-	public String hash;
-	public String previousHash;
-	private String data; // A simple message.
-	private long timeStamp; //as number of milliseconds since 1/1/1970.
+    public String hash;
+    public String previousHash;
+    private String data; // A simple message.
+    private long timeStamp; // as number of milliseconds since 1/1/1970.
+    private int nonce;
 
-	//Block Constructor.
-	public Block(String data,String previousHash ) {
-		this.data = data;
-		this.previousHash = previousHash;
-		this.timeStamp = new Date().getTime();
-	}
+    // Block Constructor.
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); // Making sure we do this after we set the other values.
+    }
+
+    public String calculateHash() {
+        return StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data);
+    }
 }
